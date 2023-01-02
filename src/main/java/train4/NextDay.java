4637f2d9e48f690d0aee01084bce65728961442d
guico52 <1305487489@qq.com>
2023-01-02 09:46:52 +0800

    train4 finished

--- src/main/java/train4/NextDay.java
+++ src/main/java/train4/NextDay.java
@@ -31,6 +31,17 @@ public class NextDay {
             }
         }
         return year + "-" + month + "-" + day;
+    }
 
+    public static void main(String[] args) {
+        System.out.println("请输入年月日：");
+        int year = new java.util.Scanner(System.in).nextInt();
+        int month = new java.util.Scanner(System.in).nextInt();
+        int day = new java.util.Scanner(System.in).nextInt();
+        String nextDate = new NextDay().getNextDay(year, month, day);
+        if(nextDate.equals("输入日期不正确"))
+            System.out.println(nextDate);
+        else
+            System.out.println("下一天是：" + nextDate);
     }
 }
